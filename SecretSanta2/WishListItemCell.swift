9b892fe9506330833ef6cf24325b9229e08db6ca
dannyshiferaw <daniel.shiferaw@berkeley.edu>
2016-11-12 18:33:30 -0800

    Cal Hacks 3.0 (First time)

---
+++ SecretSanta2/WishListItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  WishListItemCell.swift
+//  SecretSanta2
+//
+//  Created by Daniel Shiferaw on 11/12/16.
+//  Copyright © 2016 Daniel Shiferaw. All rights reserved.
+//
+
+import UIKit
+
+class WishListItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
